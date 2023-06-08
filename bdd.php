353e6d15b90c3fc33d68821907593cf15512a827
SAPH <97670093+SaafuuGT@users.noreply.github.com>
2023-06-08 20:57:31 +0200

    Delete bdd.php
    ah

--- bdd.php
+++
@@ -1,7 +0,0 @@
-<?php
-$hote = 'mysql-mangaworld.alwaysdata.net';
-$utilisateur = '289768';
-$mdp = '?????';
-$nombdd = 'mangaworld_bdd';
-$bdd = new PDO("mysql:host=$hote;dbname=$nombdd", $utilisateur, $mdp);
-?>
